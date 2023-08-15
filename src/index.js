1ab9901637c3aa2ad42e4234ad15b0a913238c4b
Milan Hauth <milahu@gmail.com>
2023-08-15 22:54:30 +0200

    depgraph: fix depPath

--- src/index.js
+++ src/index.js
@@ -124,6 +124,8 @@ async function getDepgraph(lockfilePath) {
       integrity: '',
     } : depPath[depPath.length - 1]
 
+    if (isRootPkg) depPath[0] = dep;
+
     /* this would deduplicate
     if (!_seen) { _seen = new Set() }
     if (_seen.has(depgraphData)) { return }
@@ -157,7 +159,7 @@ async function getDepgraph(lockfilePath) {
       }
     }
 
-    await enter(dep, recurse, depPath.concat([dep]));
+    await enter(dep, recurse, depPath);
   }
 
   return [
