40b75c464695a856e2270f930459ce85ce4ca9cb
Milan Hauth <milahu@gmail.com>
2023-08-15 22:12:46 +0200

    depgraph: fix depPath[0]

--- src/index.js
+++ src/index.js
@@ -127,6 +127,8 @@ async function getDepgraph(lockfilePath) {
       integrity,
     }
 
+    if (isRootPkg) depPath[0] = dep;
+
     /* this would deduplicate
     if (!_seen) { _seen = new Set() }
     if (_seen.has(depgraphData)) { return }
